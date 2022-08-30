41fae6a80c3fade190012dab0009445181dc45ee
seo-rio <scs9310@gmail.com>
2022-08-31 00:32:11 +0900

    # 개발 완료 - 예제 도메인 생성

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,22 +1,48 @@
 package study.datajpa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA이기 때문에 기본 생성자 필요 protected는 아무곳에서나 접근하지 못하게 하기 위해 JPA 스펙상 필요 (private은 안됨.)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {} // JPA이기 때문에 기본 생성자 필요 protected는 아무곳에서나 접근하지 못하게 하기 위해 JPA 스펙상 필요 (private은 안됨.)
+    @ManyToOne(fetch = FetchType.LAZY) // 다:1 관계 (하나의 회원은 팀 하나에 속함)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) { // team이 null이 아닌 경우에만 팀 변경
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
